2fb0e9ad8cc9e4f5d06807873958a3b5a9434d41
Joshua Osburn <joshuakosburn@gmail.com>
2021-11-08 17:52:46 -0500

    Removed static test route and controller.

--- controllers/products.js
+++ controllers/products.js
@@ -4,12 +4,6 @@ const Product = require('../models/Product');
 // POST
 // PATCH - Remember Idempotency!
 // DELETE
-const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ price: { $gt: 30 } })
-                    .sort('price')
-                    .select('name price');
-    res.status(200).json({ products, nbHits: products.length });
-}
 
 const getAllProducts = async (req, res) => {
     // Destructure specific values in the query.
--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,5 @@ const router = express.Router();
 const { getAllProducts, getAllProductsStatic } = require('../controllers/products');
 
 router.route('/').get(getAllProducts);
-router.route('/static').get(getAllProductsStatic);
 
 module.exports = router;
